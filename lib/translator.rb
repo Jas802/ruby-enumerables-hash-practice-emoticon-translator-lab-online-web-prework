1fd9b26366a7bedd6ed09fbe160645ff98eeea69
Jas Spencer <jas.spencer414@gmail.com>
2019-09-05 17:34:56 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,8 +16,9 @@ def load_library
     new_hash  # code goes here
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(file_path, emoticon)
+  result = load_library(file_path)['get_emoticon'][emoticon]
+  result ? result : "Sorry, that emoticon was not found"# code goes here
 end
 
 def get_english_meaning
